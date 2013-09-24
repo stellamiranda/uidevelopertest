36b5dadc1938cc4e17b0cda21acb53bcdbd3541e
Stella Maria Miranda Garcia <stellamariam@gmail.com>
2013-09-23 19:31:41 -0500

    testing modernizr

---
+++ test.php
@@ -0,0 +1,45 @@
+
+
+<html>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<link rel="stylesheet" type="text/css" href="stylesheets/global.css">
+	<!--[if lt IE 9]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+<![endif]-->
+<script type="text/javascript" src="[JS library]"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+  <script type="text/javascript" src="javascripts/selectivizr.js"></script>
+  <script type="text/javascript" src="javascripts/Modernizr.js"></script>
+  <noscript><link rel="stylesheet" href="[fallback css]" /></noscript>
+<![endif]-->
+	</head>
+
+	<body>
+
+
+		<header>
+			<link rel="shortcut icon" href="favicon.ico">
+<link rel="icon" type="image/png" href="images/favicon.png">
+			<div class="wrapper">
+				<h1>UI DeveloperTest</h1>
+			</div>
+		</header>
+		<div class = "page-wrap">
+			<div class="floater">
+
+				<div class="search_content">
+					<input type="text" class="search_textfield">
+					<input type="Submit" class="button" value="" >
+				</div>
+			</div>
+		</div>
+		<footer class="site-footer">
+			<div class="wrapper">
+  			<p>Stella Miranda, 2013 - Built using the API from <a href="http://www.themoviedb.org" target="_blank">themoviedb.org</a>, using HTML5 and CSS3.</p>  		</div>
+		</footer>
+	</body>
+</html>
