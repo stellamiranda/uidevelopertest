97eb6a8e12b950f822c0344630ff511d13be02bf
Stella Maria Miranda Garcia <stellamariam@gmail.com>
2013-09-23 21:06:31 -0500

    another test

---
+++ results.php
@@ -0,0 +1,72 @@
+<html>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<link rel="stylesheet" type="text/css" href="stylesheets/global.css">
+	<script type="text/javascript" href="javascripts/main.js"></script>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	</head>
+
+	<body>
+		<header class="results">
+			<div class="wrapper">
+				<h1><img src="images/logo.png"></h1>
+			</div>
+		</header>
+		<div class = "page-wrap">
+		
+				<div class="content">
+					<div class="wrapper">
+						<h2> Search Results</h2>
+						<div id="search_results">
+
+							<div class="item">
+								<img src="images/movie_example.jpg"/>
+								<ul>
+									<li>Star Wars Episode III</li>
+									<li>2005</li>
+									<li>Director: George Lucas</li>
+									<li>Starts: Hayden Christesen, Natalie Portman, Ewan McGregor</li>
+								</ul>
+							</div>
+							<div class="item">
+								<img src="images/movie_example.jpg"/>
+								<ul>
+									<li>Fast & Furious 6</li>
+									<li>2013</li>
+									<li>Director: Justin Lin</li>
+									<li>Start: Vin Diesel, Paul Walker, Dwayne Johnson</li>
+								</ul>
+							</div>
+							<div class="item">
+								<img src="images/movie_example.jpg"/>
+								<ul>
+									<li>Star Wars Episode III</li>
+									<li>2005</li>
+									<li>Director: George Lucas</li>
+									<li>Starts: Hayden Christesen, Natalie Portman, Ewan McGregor</li>
+								</ul>
+							</div>
+							<div class="item">
+								<img src="images/movie_example.jpg"/>
+								<ul>
+									<li>Fast & Furious 6</li>
+									<li>2013</li>
+									<li>Director: Justin Lin</li>
+									<li>Start: Vin Diesel, Paul Walker, Dwayne Johnson</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+			
+			</div>
+		</div>
+		<footer class="site-footer">
+			<div class="wrapper">
+  			<p>Stella Miranda, 2013 - Built using the API from <a href="http://www.themoviedb.org" target="_blank">themoviedb.org</a>, using HTML5 and CSS3.</p>  		</div>
+		</footer>
+	</body>
+</html>
--- test.php
+++ test.php
@@ -1,5 +1,3 @@
-
-
 <html>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -7,33 +5,22 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 	<link rel="stylesheet" type="text/css" href="stylesheets/global.css">
-	<!--[if lt IE 9]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-<![endif]-->
-<script type="text/javascript" src="[JS library]"></script>
-<!--[if (gte IE 6)&(lte IE 8)]>
-  <script type="text/javascript" src="javascripts/selectivizr.js"></script>
-  <script type="text/javascript" src="javascripts/Modernizr.js"></script>
-  <noscript><link rel="stylesheet" href="[fallback css]" /></noscript>
-<![endif]-->
+	<script type="text/javascript" href="javascripts/main.js"></script>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="icon" type="image/png" href="images/favicon.png">
 	</head>
 
 	<body>
-
-
 		<header>
-			<link rel="shortcut icon" href="favicon.ico">
-<link rel="icon" type="image/png" href="images/favicon.png">
 			<div class="wrapper">
-				<h1>UI DeveloperTest</h1>
+				<h1><img src="images/logo.png"></h1>
 			</div>
 		</header>
 		<div class = "page-wrap">
 			<div class="floater">
-
 				<div class="search_content">
 					<input type="text" class="search_textfield">
-					<input type="Submit" class="button" value="" >
+					<input type="Submit" class="button" value="" id="search" onclick="location.href='results.php';" >
 				</div>
 			</div>
 		</div>
