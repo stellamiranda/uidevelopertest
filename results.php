7db5d03e64517de55bb6cc058d15feb877b7fc53
Stella Maria Miranda Garcia <stellamariam@gmail.com>
2013-09-23 21:12:10 -0500

    fixing doctype

--- results.php
+++ results.php
@@ -1,15 +1,12 @@
+<!DOCTYPE html>
 <html>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-	<link rel="stylesheet" type="text/css" href="stylesheets/global.css">
-	<script type="text/javascript" href="javascripts/main.js"></script>
-	<link rel="shortcut icon" href="favicon.ico">
-	<link rel="icon" type="image/png" href="images/favicon.png">
+		<title>UI Developer Test</title>
+		<link rel="stylesheet" type="text/css" href="stylesheets/global.css">
+		<script type="text/javascript" href="javascripts/main.js"></script>
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="icon" type="image/png" href="images/favicon.png">
 	</head>
-
 	<body>
 		<header class="results">
 			<div class="wrapper">
@@ -67,6 +64,9 @@
 		<footer class="site-footer">
 			<div class="wrapper">
   			<p>Stella Miranda, 2013 - Built using the API from <a href="http://www.themoviedb.org" target="_blank">themoviedb.org</a>, using HTML5 and CSS3.</p>  		</div>
+		</footer>
+	</body>
+</html>edb.org" target="_blank">themoviedb.org</a>, using HTML5 and CSS3.</p>  		</div>
 		</footer>
 	</body>
 </html>
